ad6b3291061b30d0afc77556791e7e5e0b0adae8
Chuck LU <chuck.lu@edenred.com>
2020-07-08 10:44:48 +0800

    initial version

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                string fileName = "NLogTest.exe";
+                int number = 100;
+                for (int i = 0; i < number; i++)
+                {
+                    ProcessHelper.Start(fileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class ProcessHelper
+    {
+        public static void Start(string fileName)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true, 
+                UseShellExecute = false, 
+                FileName = fileName
+            };
+            var process = Process.Start(processStartInfo);
+            if (process == null)
+            {
+                Console.WriteLine("Process start failed");
+            }
+            else
+            {
+                if (process.HasExited)
+                {
+                    Console.WriteLine($"process.HasExited = {process.HasExited}, process.ExitCode = {process.ExitCode}");
+                }
+                else
+                {
+                    Console.WriteLine($"process.HasExited = {process.HasExited}");
+                }
+            }
+        }
+    }
+}
---
+++ NLogTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NLogTest
+{
+    class Program
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    logger.Info($"this is {i + 1} line");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
